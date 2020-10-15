2b6c74f789bf439d396cbc3e03ff15d6503dae88
drf <dany.francisco@outlook.com>
2020-10-15 14:52:46 +0200

    chore(my-react): Project in react for school

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.scss'
+
+const App = () => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
