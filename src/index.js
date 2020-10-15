cdb99418f9fac5a706ca60e24cb90e213385a4ba
drf <dany.francisco@outlook.com>
2020-10-15 17:04:00 +0200

    feat(home): add a list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './index.scss'
+import Home from './components/home';
+import './index.scss';
 
-const App = () => {
-  return(
-    <div>
+const data = [{
+  id: '12345',
+  name: 'Cyril',
+  phone: '0606060606',
+  key: 0,
+}, {
+  id: '12345',
+  name: 'Cyril',
+  phone: '0606060606',
+  key: 1,
+}, {
+  id: '12345',
+  name: 'Cyril',
+  phone: '0606060606',
+  key: 2,
+}];
 
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    {data.map((item) => <Home key={item.key} data={item} />)}
+  </div>
+);
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
